86b7d16991cb72cb875eea87d2b9a3b3a67cd412
fanwencong <1009645313@qq.com>
2019-09-21 14:34:36 +0800

    modify config.yml

--- src/main/java/com/fanwencong/configserver/ConfigServerApplication.java
+++ src/main/java/com/fanwencong/configserver/ConfigServerApplication.java
@@ -1,18 +1,22 @@
 package com.fanwencong.configserver;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.annotation.Configuration;
 
+@Slf4j
 @Configuration
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
-
+        long start = System.currentTimeMillis();
         SpringApplication.run(ConfigServerApplication.class, args);
+        log.info("===========Started ConfigServer successfully in {} seconds =============",(System.currentTimeMillis()-start)/1000);
+
     }
 
 }
